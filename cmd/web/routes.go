f91794bd7157c88cb97add9c870a6eafdb1f90be
alexandru calin <alexandrucalin@proton.me>
2024-10-26 14:45:37 +0300

    add middleware to log HTTP requests

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,11 +2,11 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /code/view/{id}", codeView)
 
-	return mux
+	return app.logRequest(mux)
 }

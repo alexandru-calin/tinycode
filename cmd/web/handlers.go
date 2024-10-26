1f64a5a65d3f2d3246d49416fd0c6cab51967812
alexandru calin <alexandrucalin@proton.me>
2024-10-26 12:01:53 +0300

    add /code/view/:id route for displaying specific code snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,8 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to tinycode!")
+}
+
+func codeView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, "Malformed ID", http.StatusBadRequest)
+		return
+	}
+
+	message := fmt.Sprintf("Viewing code snippet with ID %d", id)
+	fmt.Fprintln(w, message)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /code/view/{id}", codeView)
 
 	log.Println("Starting the server on port 3000")
 	http.ListenAndServe(":3000", mux)

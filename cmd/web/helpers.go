c385253f96e1e03e5c7b92a6dd30c73ec07e10e3
alexandru calin <alexandrucalin@proton.me>
2024-10-28 18:15:46 +0200

    modify render method to catch runtime errors

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 )
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, page string, data any) {
-	tpl := app.templateCache[page]
+	tpl, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
 
-	err := tpl.ExecuteTemplate(w, "base", data)
+	buf := new(bytes.Buffer)
+
+	err := tpl.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
+		return
 	}
+
+	buf.WriteTo(w)
 }
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
